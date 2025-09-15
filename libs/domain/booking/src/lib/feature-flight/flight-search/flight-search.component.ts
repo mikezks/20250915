6e8e775e41c60aaac8879cc62248696fb7f2ee29
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-15 12:22:16 +0200

    refactor: explicit effect

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal, untracked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
@@ -34,7 +34,10 @@ export class FlightSearchComponent {
 
   constructor() {
     effect(() => console.log(this.route()));
-    effect(() => this.search());
+    effect(() => {
+      this.filter();
+      untracked(() => this.search());
+    });
   }
 
   protected search(): void {
