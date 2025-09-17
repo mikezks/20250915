047e3b8f32366821da7303c2c9d831c907f04968
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-09-17 11:28:09 +0200

    refactor: validation required, custom validator passenger status

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,12 +1,28 @@
 import { httpResource } from '@angular/common/http';
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
-import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { Control, form } from '@angular/forms/signals';
+import { Component, effect, input, numberAttribute, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Control, customError, form, required, schema, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger';
-import { validatePassengerStatus } from '../../util-validation';
+
 
 // (3) Field Logic: required, custom validator, disabled, hidden, readonly, etc.
+const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.name);
+  validate(passengerPath.passengerStatus, ({ value }) => {
+    const validStatus = ['A', 'C'];
+
+    if (!validStatus.includes(value())) {
+      return customError({
+        kind: 'passengerStatus',
+        message: 'The field name has a passengerStatus error - please fix this!'
+      });
+    }
+
+    return undefined;
+  });
+});
+
 
 @Component({
   selector: 'app-passenger-edit',
@@ -23,7 +39,7 @@ export class PassengerEditComponent {
   private passenger = signal(initialPassenger);
 
   // (2) Field State: Meta Data - valid, dirty, touched, etc.
-  protected editForm = form(this.passenger);
+  protected editForm = form(this.passenger, passengerSchema);
 
   id = input(0, { transform: numberAttribute });
   protected passengerResource = httpResource<Passenger>(() => ({
